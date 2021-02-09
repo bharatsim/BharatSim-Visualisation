875447994760329815e7f9639d4ffa898d211472
Chandrakant Kaski <kaskichandrakant@gmail.com>
2021-02-09 17:55:22 +0530

    BHA-199 |update cleanup script to delete yarn cache from all| Chandrakant/Ganesh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -14,12 +14,18 @@ mv ./backend/public/index.html .
 rm -rf ./backend/public/*
 mv index.html ./backend/public/
 
-# clean node_modules
+# clean node_modules and cache
 
 if [[ $1 == --deep-clean ]]; then
     echo "Cleaning  node_modules"
     rm -rf frontend/node_modules
     rm -rf backend/node_modules
+    rm -rf node_modules
+    echo "Cleaning yarn cache for all"
+    yarn cache clean
+    cd frontend && yarn cache clean
+    cd ..
+    cd backend && yarn cache clean
 else
     echo "node_modules are not cleared"
 fi
